0412c5195170a69de99a2affc684663458053b93
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-08 15:44:06 -0400

    greeting complete

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const name = "Carl";
+const amount = 100
 
 app.get('/greeting',(req, res)=>
 {
-    res.send(`<h2>${name}! You're finally awake! Walked right into that Imperial ambush.</h2>`)
+    res.send(`<h2>You're finally awake! Walked right into that Imperial ambush.</h2>`)
 })
 
+app.get('/greeting/:name',(req, res)=>
+{
+    res.send(`<h2>${req.params.name} is your name? Alright, well follow me!</h2>`)
+})
+
+app.get('/tip',(req, res)=>
+{
+    res.send(`${amount}`)
+})
 
 app.listen("3000", (req, res) => {
     console.log(`Server Listening on Port 3000`);
