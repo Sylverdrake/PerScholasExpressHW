a8d3f5d06883fd7cdcdf2a889aa2de297fc44480
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-08 15:53:42 -0400

    tip calc done

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const amount = 100
-
 app.get('/greeting',(req, res)=>
 {
     res.send(`<h2>You're finally awake! Walked right into that Imperial ambush.</h2>`)
@@ -14,9 +12,9 @@ app.get('/greeting/:name',(req, res)=>
     res.send(`<h2>${req.params.name} is your name? Alright, well follow me!</h2>`)
 })
 
-app.get('/tip',(req, res)=>
+app.get('/tip/:amount/:percent',(req, res)=>
 {
-    res.send(`${amount}`)
+    res.send(`$${req.params.amount} is the amount. ${req.params.percent}% is the tip. Expected tip is $${req.params.amount * req.params.percent/100}`);
 })
 
 app.listen("3000", (req, res) => {
